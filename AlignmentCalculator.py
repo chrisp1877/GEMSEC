f83c93b20798ccc76cde9fa93fd344c1fee1a139
Chris Pecunies <chrisp1877@gmail.com>
2019-02-21 17:10:29 -0800

    updated

--- AlignmentCalculator.py
+++ AlignmentCalculator.py
@@ -1,144 +1,89 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 29 17:33:56 2019
+# Author: Chris Pecunies
 
-@author: Chris
-"""
-
-import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import ElasticNet
-from sklearn.model_selection import GridSearchCV
+import numpy as np
 
 class AlignmentCalculator:
-    p = pd.DataFrame()
-    b = pd.DataFrame()
-    peptides = []
-    binders = []
-    cluster_keys = []
-    tss_df = pd.DataFrame()
-    matrices = {}
     
-#    Peptide and binder sequence length must be the same
-#    Format of peptides and binders csv:
-#    0  1  2  3  4  5  6  7  8  ...  n (peptide and binder length the same)
-#    A  I  I  D  Y  I  A  Y  M  ...  S
-#    A  I  Y  D  T  M  Q  Y  V  ...  G
     
-    def __init__(self, p_path = None, b_path = None):
+    # Takes in DataFrame from csv (n rows x l columns) as binding peptides, DataFrame from csv (mrows x l columns)
+    # as peptides of interest, and constructs an AlignmentCalculator object with cluster keys, corresponding distance
+    # matrices (in corresponding directory, with corresponding file name), peptides DataFrame (m x l), binding DataFrame (n x l)
+    # !! Input csvs must have first row be index columns, not the first peptide sequence
+    def __init__(self, p = None, b = None):
         self.cluster_keys = ["a", "b", "c", "e", "h", "i", "p", "r"]
-        self.dist_matrices = dict.fromkeys(self.cluster_keys)
+        self.amino_acids = ["A", "R", "N", "D", "C", "E", "Q", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T","W","Y","V"]
+        self.matrices = dict.fromkeys(self.cluster_keys)
         for key in self.cluster_keys:
-            self.matrices[key] = pd.read_csv("./dist_matrices/Euclidean_AA_scaled_cluster_ " + key + ".csv", index_col = 0)
-        self.__store_values(p_path, b_path)
-
-    def __store_values(self, p_path, b_path):
-        if p_path is not None:
-            self.p = pd.read_csv(p_path)
-            self.length = len(list(self.p.columns))
-            self.peptides = [''.join(list(self.p.iloc[m, :])) for m in range(len(list(self.p.index)))]
-        if b_path is not None:
-            self.b = pd.read_csv(b_path)
-            self.length = len(list(self.b.columns))
-            self.binders = [''.join(list(self.b.iloc[n, :])) for n in range(len(list(self.b.index)))]
-        
-    def set_peptides(self, p_path):
-        self.__store_values(p_path, None)
+            self.matrices[key] = pd.read_csv("cluster_Euclidean\improved\cluster_" + key + ".csv", index_col = 0)
+        if p is not None and b is not None:
+            if len(list(p.columns)) != len(list(b.columns)):
+                raise ValueError("Peptide length must be equal to compare")  
+            self.binders = b
+            self.peptides = p
+            self.num_of_peptides = len(list(self.peptides.index))
+            self.num_of_binders = len(list(self.binders.index))
+            self.length = len(list(self.peptides.columns))
+            self.peptide_strings = [None] * self.num_of_peptides
+            self.binder_strings = [None] * self.num_of_binders
+            self.storePeptideAndBinderStrings()
     
-    def set_binders(self, b_path):
-        self.__store_values(None, b_path)
+    def storePeptideAndBinderStrings(self):
+        for m in range(self.num_of_peptides):
+            peptide_string = ''.join(list(self.peptides.iloc[m, :]))
+            self.peptide_strings[m] = peptide_string
+        for n in range(self.num_of_binders):
+            binder_string = ''.join(list(self.binders.iloc[n, :]))
+            self.binder_strings[n] = binder_string
+            
+            print("Peptide " + str(m) + " of " + str(self.num_of_peptides) + " done")
+            
+    #takes in one-letter string 1, one-letter string 2, compares and outputs distance from distance matrix of cluster given by user.
+    #This method is non-dependent on peptide and binder list AlignmentCalculator is constructed with
+    def calculateAASimilarity(self, AA1, AA2, cluster):
+        return self.matrices[cluster].loc[AA1, AA2]
+
+    #pass in string 1 (peptide 1) and string 2 (peptide 2) and string for cluster, outputs sum of distances from distrance matrix
+    #This method is non-dependent on peptide and binder list AlignmentCalculator is constructed with
+    def calculateSequenceSimilarity(self, PEP1, PEP2, cluster):
+        score = 0
+        for l in range(self.length):
+            score += self.calculateAASimilarity(PEP1[l], PEP2[l], cluster)
+        return score
+    
+    # Takes in petide of interest (n rows x 12 columns), binding peptides for comparison (m rows x 12 columns) and outputs
+    # a dataframe outputting similarity scores for each of n peptides of interest for each of the m binding peptides for
+    # comparison, returns m x n data frame with similarity scores between each peptide and binder
+    def calculateAlignment(self, cluster):
+        alignment_scores = pd.DataFrame(np.zeros(shape=(self.num_of_peptides, self.num_of_binders)))
         
-#    Format of TSS.csv file must be:
-#               a    b    c    ...  r   
-#    AIIDYIAYM  574  641  662  ...  599
-#    AIYDTMQYV  612  690  742  ...  640
-#    ALATFTVNI  719  782  829  ...  744  
+        for m in range(self.num_of_peptides):
+            peptide_string = ''.join(list(self.peptides.iloc[m, :]))
+            for n in range(self.num_of_binders):
+                binder_string = ''.join(list(self.binders.iloc[n, :]))
+                alignment_scores.iloc[m,n] = self.calculateSequenceSimilarity(peptide_string, binder_string, cluster)
+        alignment_scores.index = self.peptide_strings
+        alignment_scores.columns = self.binder_strings
+        return alignment_scores
+    
+    # Returns a m x n dataframe which provides the sum of scores for the calculateAlignment method
+    # for each of the cluster keys
+    def calculateTotalAlignment(self):
+        alignment_scores = self.calculateAlignment(self.cluster_keys[0])
+        for i in range(1, len(self.cluster_keys)):
+            alignment_scores += self.calculateAlignment(self.cluster_keys[i])   
+        return alignment_scores
+    
+    def calculateTotalSimilarityScores(self):
+        similarity_scores = pd.DataFrame(np.zeros(shape=(self.num_of_peptides, len(self.cluster_keys))))
+        similarity_scores.index = self.peptide_strings
+        similarity_scores.columns = self.cluster_keys
         
-    def import_TSS(self, TSS_path):
-        self.tss_df = pd.read_csv(TSS_path)
-        self.tss_df.set_index('Unnamed: 0', inplace = True)
-        self.peptides = list(self.tss_df.index)
-        
-    def calculate_TSS(self):
-        if self.p is None or self.b is None:
-            raise Exception("List of peptides or binders not set")
-        if len(list(self.p.columns)) != len(list(self.b.columns)):
-            raise Exception("Sequence length of peptides and binders must be equal")
-        np_ss = np.zeros(shape=(len(self.peptides), len(self.cluster_keys)))
-        for m in range(len(self.peptides)):
-            for i, key in enumerate(self.cluster_keys):
-                total_score = 0
-                for n in range(len(self.binders)):
-                    total_score += sum(self.matrices[key].loc[self.peptides[m][l],
-                                       self.binders[n][l]] for l in range(self.length)
-                                       if self.peptides[m] is not self.binders[n])
-                np_ss[m][i] = total_score
-            print(str(m), str(len(self.peptides)))
-        similarity_scores = pd.DataFrame(np_ss, index = self.peptides, columns = self.cluster_keys)
-        self.tss_df = similarity_scores
+        for m in range(self.num_of_peptides): #iterates thru rows of dataframe
+            for key in self.cluster_keys: #iterates thru columns, keys, of dataframe
+                totalScore = 0
+                for n in range(self.num_of_binders): #calculates similarity score for given peptide and every binder, sums and puts in column
+                    totalScore += self.calculateSequenceSimilarity(self.peptides.iloc[m,:], self.binders.iloc[n, :], key)  
+                similarity_scores[key].iloc[m] = totalScore
+            print("Peptide " + str(m) + " of " + str(self.num_of_peptides) + " done")
         return similarity_scores
-    
-#    Format of binding affinity data must be:
-#    Sequence   Binding Affinity
-#    FLIYFRSPL  -1.04139
-#    RLDPRLAPV  -1.04139
-#    FLMQIAILV  -1.04374
-    
-    def lin_reg_predict(self, binding_affinity_data_path):
-        if self.tss_df.empty:
-            self.calculate_TSS()
-        X = self.tss_df
-        scaler = MinMaxScaler(feature_range=(-1,1),copy=True)
-        X = pd.DataFrame(scaler.fit_transform(X, [-1,1]), index = X.index, columns = X.columns)
-        Y = pd.read_csv(binding_affinity_data_path, header = None)
-        Y.columns = Y.iloc[0]
-        Y = Y.drop(0)
-        Y.set_index('Sequence',inplace=True)
-        reg = LinearRegression().fit(X, Y)
-        prediction = reg.predict(X)
-        X['prediction'] = prediction
-        data = np.zeros([len(self.peptides), 2])
-        data[:,0] = Y['Binding Affinity']
-        data[:,1] = X['prediction']
-        score = np.corrcoef(data[:,0], data[:,1])[0, 1]
-        predictions = pd.DataFrame(data, index = X.index, columns = ['Binding Affinity', 'Predicted'])
-        return predictions, score
-    
-    def elastic_net_predict(self, binding_affinity_data_path):
-        if self.tss_df.empty:
-            self.calculate_TSS()
-        X = self.tss_df
-        scaler = MinMaxScaler(feature_range = (-1,1), copy = True)
-        X = pd.DataFrame(scaler.fit_transform(X, [-1,1]), index = X.index, columns = X.columns)
-        Y = pd.read_csv(binding_affinity_data_path, header = None)
-        Y. columns = Y.iloc[0]
-        Y = Y.drop(0)
-        Y.set_index('Sequence', inplace = True)
-        eNet = ElasticNet(tol=0.1)
-        parametersGrid = {"max_iter": [1, 5, 10],
-                      "alpha": [0.01, 0.1, 1, 10, 100],
-                      "l1_ratio": np.arange(0.0, 1.0, 0.1)}
-        grid = GridSearchCV(eNet, parametersGrid, scoring='r2', cv=10)
-        grid.fit(X, Y)
-        prediction = grid.predict(X)
-        X['prediction'] = prediction
-        data = np.zeros([len(self.peptides), 2])
-        data[:,0] = Y['Binding Affinity']
-        data[:,1] = X['prediction']
-        score = np.corrcoef(data[:,0], data[:,1])[0, 1]
-        predictions = pd.DataFrame(data, index = X.index, columns = ['Binding Affinity', 'Predicted'])
-        return predictions, score
-    
-'''
-To run with already populated TSS matrix:
-
-from AlignmentCalculator import AlignmentCalculator
-
-ac = AlignmentCalculator()
-ac.import_TSS('TSS_scores.csv')
-ac.set_binders('top_binders.csv')
-predicted = ac.lin_reg_predict('largest_mhc0_1.csv')[0]
-score = ac.lin_reg_predict('largest_mhc0_1.csv')[1]
-'''
